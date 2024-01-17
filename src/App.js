c55bad12b10cce58e545ede96d3a4661ae5c1a82
Vitor Migoto <vitormigoto@gmail.com>
2024-01-17 12:25:47 -0300

    feat:separando posts em componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    posts : []
+  };
+
+  // const [posts, setPosts] = useState([]);
+
+  componentDidMount(){
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
+  }
+
+  render(){
+    const { posts } = this.state;
+
+    return (
+      <section className='container'>
+        <div className='posts'>
+           {posts.map(post => (     
+              <PostCard post={post} key={post.id}/>
+           ))}
+        </div>
+      </section>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export const PostCard = ({post}) => {
+  
+    return (
+        <div className='post'>        
+            <img src={post.cover} alt = {post.title} />
+            <div className='post-content'>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+            </div>              
+        </div>
+    );
+}
